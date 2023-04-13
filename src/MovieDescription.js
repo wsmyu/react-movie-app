b451fc2e61cf9082b6cb5a9e7b098bc2f707f010
Wing Suet Michelle Yu <n01535631@humbermail.ca>
2023-04-13 12:51:48 -0400

    Modify review

--- src/MovieDescription.js
+++ src/MovieDescription.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { useParams,useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-
 function MovieDescription({ addToFavorites,isViewingMovie,setIsViewingMovie }) {
   const { imdbID } = useParams();
   const [movie, setMovie] = useState('');
   const [reviewText, setReviewText] = useState('');
   const [reviews, setReviews] = useState([]);
   const [nameText, setNameText] = useState(''); 
+  const [responseText, setResponseText] = useState('');
+  const [responseNameText, setResponseNameText] = useState('');
 
 
   const handleNameChange = (e) => {
     setNameText(e.target.value);
   };
-
+  const handleResponseNameChange = (e) => {
+    setResponseNameText(e.target.value);
+  };
   const handleAddToFavorites = () => {
     const movieDetails = {
       title: movie.Title,
@@ -36,7 +39,8 @@ function MovieDescription({ addToFavorites,isViewingMovie,setIsViewingMovie }) {
     const newReview = {
       name: nameText,
       text: reviewText,
-      date: new Date().toLocaleDateString()
+      date: new Date().toLocaleDateString(),
+      responses: []
     };
     const storedReviews = JSON.parse(localStorage.getItem(imdbID)) || [];
     const newReviews = [...storedReviews, newReview];
@@ -47,6 +51,31 @@ function MovieDescription({ addToFavorites,isViewingMovie,setIsViewingMovie }) {
   }
   };
 
+
+const handleResponseChange = (e) => {
+  setResponseText(e.target.value);
+};
+
+const handleAddResponse = (e, reviewIndex) => {
+  e.preventDefault();
+  if (responseNameText.trim() !== '' && responseText.trim() !== '') {
+    const storedReviews = JSON.parse(localStorage.getItem(imdbID)) || [];
+    const reviewToUpdate = storedReviews[reviewIndex];
+  
+    if (!reviewToUpdate.responses) {
+      reviewToUpdate.responses = [];
+    }
+    const newResponse = {
+      name: responseNameText,
+      text: responseText
+    };
+    reviewToUpdate.responses.push(newResponse);
+    localStorage.setItem(imdbID, JSON.stringify(storedReviews));
+    setReviews(storedReviews);
+    setResponseText('');
+    setResponseNameText('');
+  }
+};
   useEffect(() => {
     const storedReviews = JSON.parse(localStorage.getItem(imdbID));
     if (storedReviews) {
@@ -64,76 +93,111 @@ function MovieDescription({ addToFavorites,isViewingMovie,setIsViewingMovie }) {
     }
   }, [imdbID, isViewingMovie]);
   
-  
-
- 
       return (
-        
+       <>
+       {isViewingMovie ? (
         <div className='movieDescription'>
-        <img src={movie.Poster} />
-        <div className='movieDetails'>
-          <h1 style={{ marginBottom: "1rem" }}>{movie.Title}</h1>
-      
-          <p>Released Year: {movie.Year}</p>
-          <p>Actors: {movie.Actors}</p>
-          <p>Director:  {movie.Director}</p>
-
-          <p>Genre: {movie.Genre}</p>
-          <p>Awards: {movie.Awards}</p>
-
-          <p>{movie.Plot}</p>
-          <button className="btn btn-outline-success addFavourite" onClick={handleAddToFavorites}>
-            Add to Favorites
-          </button>
-
-          <hr />
-          <h2>Audience reviews</h2>
-          <div className='reviewDisplay'>
-            {reviews.length > 0 ? (
-                reviews.map((review, index) => (
-                  <div className="review" key={index}>
-                    <p className='user-name'>{review.name}</p>
-                    <p className="review-text">{review.text}</p>
-                    <p className="review-date">{review.date}</p>
+    <img src={movie.Poster} />
+    <div className='movieDetails'>
+      <h1 style={{ marginBottom: "1rem" }}>{movie.Title}</h1>
+      <p>Released Year: {movie.Year}</p>
+      <p>Actors: {movie.Actors}</p>
+      <p>Director:  {movie.Director}</p>
+      <p>Genre: {movie.Genre}</p>
+      <p>Awards: {movie.Awards}</p>
+      <p>{movie.Plot}</p>
+      <button className="btn btn-outline-success addFavourite" onClick={handleAddToFavorites}>
+        Add to Favorites
+      </button>
+      <hr />
+      <h2>Audience Reviews</h2>
+    <div className='reviewDisplay'>
+      {reviews && reviews.length > 0 ? (
+        reviews.map((review, reviewIndex) => (
+          <div className="review" key={reviewIndex}>
+            <p className='user-name'>{review.name}</p>
+            <p className="review-text">{review.text}</p>
+            <p className="review-date">{review.date}</p>
+            {review.responses && review.responses.length > 0 && (
+              <div className="responses">
+                <h4 style={{color:"#2e303b"}}>Replies:</h4>
+                {review.responses.map((response, responseIndex) => (
+                  <div className="response" key={responseIndex}>
+                    <p className='user-name'>{response.name}</p>
+                    <p className="response-text">{response.text}</p>
                   </div>
-                ))
-              ) : (
-                <p>No reviews yet.</p>
-              )}
+                ))}
+              </div>
+            )}
+            <form style={{marginTop:"20px",marginLeft:"30px",marginRight:"20px"}} 
+                onSubmit={(e) => handleAddResponse(e, reviewIndex)}>
+              <div>
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  id={`responseName${reviewIndex}`}
+                  value={responseNameText}
+                  placeholder='Please Enter Your Name'
+                  onChange={handleResponseNameChange}
+                  style={{width:"30%", marginBottom:"1rem"}}
+                />
+              </div>
+              <div>
+                <textarea 
+                  className="form-control" 
+                  placeholder='Join the discussion!'
+                  id={`responseInput${reviewIndex}`}
+                  rows="2"
+                  value={responseText} 
+                  onChange={handleResponseChange}
+                />
+              </div>
+              <button style={{marginTop:'10px'}} type="submit" className="btn btn-primary">Submit</button>
+            </form>
           </div>
-        <br/>
-        <h2>Reviews</h2>
-        <form onSubmit={handleAddReview}>
-          <div>
-              <input 
-                type="text" 
-                className="form-control" 
-                id="nameInput"
-                value={nameText}
-                placeholder='Please Enter Your Name'
-                onChange={handleNameChange}
-                style={{width:"30%"}} />
-          </div>
-          <div>
-            <label htmlFor="reviewInput" className="form-label">Write a review</label>
-            <textarea 
-              className="form-control" 
-              placeholder='Join the discussion!'
-              id="reviewInput" 
-              rows="3" 
-              value={reviewText} 
-              onChange={handleReviewChange}/>
-          </div>
-          <br/>
-          <button type="submit" className="btn btn-primary">Submit</button>
+        ))
+      ) : (
+        <p>No reviews yet.</p>
+      )}
+    </div>
+    <br/>
+    <h2>Create Your Own Review</h2>
+    <form onSubmit={handleAddReview}>
+      <div>
+        <input 
+          type="text" 
+          className="form-control" 
+          id={`responseName${reviews.length}`}
+          value={nameText}
+          placeholder='Please Enter Your Name'
+          onChange={handleNameChange}
+          style={{width:"30%"}} 
+        />
+      </div>
+      <div>
+        <label htmlFor="reviewInput" className="form-label">Write a review</label>
+        <textarea 
+          className="form-control" 
+          placeholder='Write some reviews'
+          id={`responseInput${reviews.length}`}  
+          rows="3" 
+          value={reviewText} 
+          onChange={handleReviewChange}
+        />
+      </div>
+          
+          <button style={{marginTop:'10px'}}type="submit" className="btn btn-primary">Submit</button>
         </form>
         
       </div>
       
     </div>
-  
+ 
+       ) :null}
+
+</>
   );
- } 
+}
 export default MovieDescription;
 
 
